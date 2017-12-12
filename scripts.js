3e9442b3a4594cd4443bfa33789b668c15a96aa6
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-12 16:07:42 -0700

    working on borders and media queries

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.tabs-conatiner .tabs a').on('click', function(e)  {
-        var currentAttrValue = $(this).attr('href');
-        $('.tabs-conatiner ' + currentAttrValue).show().siblings().hide();
+        var attrValue = $(this).attr('href');
+        $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
         $(this).parent('li').addClass('active').siblings().removeClass('active');
         e.preventDefault();
     });
