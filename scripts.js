b6b74d0269a6c00f1d59ea882f8d09633a892a67
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-12 20:58:52 -0700

    cleaned up some code

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.tabs-conatiner .tabs a').on('click', function(e)  {
-        var attrValue = $(this).attr('href');
+        let attrValue = $(this).attr('href');
         $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
         $(this).parent('li').addClass('active').siblings().removeClass('active');
         e.preventDefault();
