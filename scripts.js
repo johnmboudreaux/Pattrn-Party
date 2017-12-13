ad1f89aa3ce929b86c920eecdc833fe4823efcbf
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-13 08:44:59 -0700

    refactored some js added button styled and directed to github

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    $('.tabs-conatiner .tabs a').on('click', function(e)  {
-        let attrValue = $(this).attr('href');
-        $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
-        $(this).parent('li').addClass('active').siblings().removeClass('active');
-        e.preventDefault();
-    });
-});
+$('.tabs-conatiner .tabs a').on('click', tabSwitch);
+
+function tabSwitch(e) {
+  let attrValue = $(this).attr('data');
+  $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
+  $(this).parent('li').addClass('active').siblings().removeClass('active');
+  // e.preventDefault();
+}
