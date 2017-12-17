8e5c7317d11d68a0abea60851c353ea82ded6406
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-17 08:58:44 -0700

    added button

--- scripts.js
+++ scripts.js
@@ -4,5 +4,4 @@ function tabSwitch(e) {
   let attrValue = $(this).attr('data');
   $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
   $(this).parent('li').addClass('active').siblings().removeClass('active');
-  // e.preventDefault();
 }
