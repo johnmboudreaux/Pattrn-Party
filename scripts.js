1e4aec79d16b7152c9e26b75d29c0413d2c70039
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-02 19:12:07 -0700

    styling mobile view

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 $('.tabs-conatiner .tabs a').on('click', tabSwitch);
+$('.header-button').on('click', toggleNav);
 
 function tabSwitch(e) {
   let attrValue = $(this).attr('data');
   $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
   $(this).parent('li').addClass('active').siblings().removeClass('active');
+}
+
+function toggleNav(event) {
+  
 }
