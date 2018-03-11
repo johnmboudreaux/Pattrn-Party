a68fb8687e3a6bd25545a106e886cd53d5ab14d0
johnmboudreaux <jhnbdrx@gmail.com>
2018-03-11 14:36:49 -0600

    made changes to accordion

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $('.header-button').on('click', toggleNav);
 function tabSwitch(e) {
   let attrValue = $(this).attr('data');
   $(`.tabs-conatiner ${attrValue}`).show().siblings().hide();
-  $(this).parent('li').addClass('active').siblings().removeClass('active');
+  $(this).parent('li').addClass('active').siblings().removeClass('active selected-card');
 }
 
 function toggleNav() {
